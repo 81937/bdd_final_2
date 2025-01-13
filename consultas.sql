0fd7b83479642599fb1cd36d3af4d7789bf019fb
Carlos-Lima2510 <carlos251005.alvarado@gmail.com>
2025-01-13 21:03:12 +0100

    Add SQL queries and table definitions for reservation system

--- consultas.sql
+++ consultas.sql
@@ -149,14 +149,41 @@ BEGIN
 
 -- Intersección --
 
+SELECT * FROM reserva WHERE codigo_unico LIKE 'AB%' INTERSECT SELECT * FROM reserva WHERE costo > 100.00;
+
 
 -- Diferencia --
 
+SELECT * FROM reserva WHERE codigo_unico LIKE 'A%' EXCEPT SELECT * FROM reserva WHERE costo > 125.00;
 
 -- Unión --
 
+SELECT * FROM reserva WHERE codigo_unico LIKE '%10%' UNION SELECT * FROM reserva WHERE estado = 'Confirmada';
 
 -- Agregación --
+
+SELECT AVG(costo) FROM reserva WHERE estado = 'Pendiente';
+
+-- Trigger Invidivual --
+
+DELIMITER $$
+
+CREATE TRIGGER valida_fechas_reserva
+BEFORE INSERT ON reserva
+FOR EACH ROW
+BEGIN
+    -- Verificar que la fecha de entrada no sea posterior a la fecha de salida
+    IF NEW.fecha_entrada > NEW.fecha_salida THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: La fecha de entrada no puede ser posterior a la fecha de salida.';
+    END IF;
+END$$
+
+DELIMITER ;
+
+
+-- Trigger Grupal --
+
 
 
 
